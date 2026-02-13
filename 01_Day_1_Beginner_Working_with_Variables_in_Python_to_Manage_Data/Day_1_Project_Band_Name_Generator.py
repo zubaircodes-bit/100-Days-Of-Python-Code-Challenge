e0fc6a6f729ab172fa42ef50da554483425196de
zubaircodes-bit <zubairali.code@gmail.com>
2026-02-13 19:45:22 +0500

    Assignment 1 Completed

---
+++ 01_Day_1_Beginner_Working_with_Variables_in_Python_to_Manage_Data/Day_1_Project_Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Brand Name Generator");
+city = input("Enter the name of the city: \n");
+pet = input("Enter the name of the pet: \n");
+print("Your Brand Name is : " + city + " " + pet);
