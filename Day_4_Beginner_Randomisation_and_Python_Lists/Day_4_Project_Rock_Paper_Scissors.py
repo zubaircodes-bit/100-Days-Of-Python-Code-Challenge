3b5d8637e143ffda817fd890700d3fee048ae032
zubaircodes-bit <zubairali.code@gmail.com>
2026-02-17 11:00:27 +0500

    day 4 rock paper and scissors game

---
+++ Day_4_Beginner_Randomisation_and_Python_Lists/Day_4_Project_Rock_Paper_Scissors.py
@@ -0,0 +1,46 @@
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___) '''
+
+paper = '''
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)'''
+
+scissors = '''
+   _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)'''
+
+choices = [rock, paper, scissors]
+
+userChoice = int(input("Enter 0 for rock, 1 for paper, 2 for scissors: "))
+computerChoice = random.randint(0, 2)
+
+if userChoice < 0 or userChoice > 2:
+    print("Invalid choice!")
+else:
+    print("You chose:")
+    print(choices[userChoice])
+
+    print("Computer chose:")
+    print(choices[computerChoice])
+
+    if userChoice == computerChoice:
+        print("It's a tie!")
+    elif (userChoice == 0 and computerChoice == 2) or (userChoice == 1 and computerChoice == 0) or (userChoice == 2 and computerChoice == 1):
+        print("You win!")
+    else:
+        print("You lose!")
---
+++ Day_4_Beginner_Randomisation_and_Python_Lists/Day_4_Who will pay the bill.py
@@ -0,0 +1,4 @@
+import random
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+randomNum = random.choice(friends)
+print(randomNumin)
