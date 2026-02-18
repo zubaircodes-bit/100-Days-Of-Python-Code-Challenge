f3d38840a36cc74b106746568ef6ae5b4e22c53e
zubaircodes-bit <zubairali.code@gmail.com>
2026-02-18 15:19:17 +0500

    Pypasswrd v1

---
+++ Day_5_Beginner_Python_Loops/Day_5_Project_PyPassword_Generator.py
@@ -0,0 +1,29 @@
+import random
+letters = [
+  'a','b','c','d','e','f','g','h','i','j','k','l','m',
+  'n','o','p','q','r','s','t','u','v','w','x','y','z',
+  'A','B','C','D','E','F','G','H','I','J','K','L','M',
+  'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
+];
+
+numbers = ['1','2','3','4','5','6','7','8','9'];
+
+symbols = [
+  '!','@','#','$','%','^','&','*',
+  '(',')','-','_','=','+',
+  '[',']','{','}','|','\\',
+  ';',':','"',"'",
+  '<','>',',','.','?','/'
+];
+
+numbersCount = int(input("How many numbers would you like in your Password ? \n "))
+lettersCount = int(input("How many letters would you like in your Password ? \n "))
+symbolsCount = int(input("How many symbols would you like in your Password ? \n "))
+password = ""
+for i in range(0, numbersCount):
+  password += random.choice(numbers);
+for i in range(0, lettersCount):
+  password += random.choice(letters);
+for i in range(0, symbolsCount):
+  password += random.choice(symbols);
+print(f"Your Password is {password}.")
